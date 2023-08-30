d16afdd801f39fa7a41ba8cf50fa586c650638ce
Lance Swan <lance@mightily.com>
2023-08-30 10:45:29 -0400

    Initial deployment.

---
+++ precache-manifest.4c07ab75e7098260a236bd4669e1a6d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "888b9cc25dedfdc24c7a",
+    "url": "/static/js/main.6d48ee8a.chunk.js"
+  },
+  {
+    "revision": "6a81064e3b4189a31fd1",
+    "url": "/static/js/runtime~main.e6002803.js"
+  },
+  {
+    "revision": "0296ebba21252a6736a9",
+    "url": "/static/js/2.0f4a1175.chunk.js"
+  },
+  {
+    "revision": "d06d8fe717dc215d2ddf",
+    "url": "/static/js/3.71e6714e.chunk.js"
+  },
+  {
+    "revision": "c16ee24bab8d98d3431206a5e7aedbe3",
+    "url": "/static/media/lume-logo-white.c16ee24b.svg"
+  },
+  {
+    "revision": "82028ff7d75a420e742684878b98f4a8",
+    "url": "/index.html"
+  }
+];
